73e8ee230b7c1484b6103f1bc608d845822dd1a6
mihdjo <mihailo.djokovic2003@gmail.com>
2026-03-09 17:53:37 +0100

    Automatizovani testovi - konacne izmene

--- backend/tests_ci.py
+++ backend/tests_ci.py
@@ -3,10 +3,15 @@ from django.test import TestCase
 class SimpleCIBuildTest(TestCase):
     def test_math_works(self):
         """Osnovna provera da li Python uopšte radi"""
+        print("-----------------------------------------------------------------------")
+        print("\n[TEST 1/2] Provera Python okruženja: 1+1=2... USPEŠNO!")
         self.assertEqual(1 + 1, 2)
+        print("-----------------------------------------------------------------------")
 
     def test_homepage_response(self):
         """Proverava da li početna stranica odgovara"""
+        print("-----------------------------------------------------------------------")
+        print("[TEST 2/2] Kucam na vrata servera... SERVER JE BUDAN I ODGOVARA!")
         response = self.client.get('/')
-        # Čak i ako dobijemo 404, to znači da Django radi i odgovara!
         self.assertIn(response.status_code, [200, 404])
+        print("-----------------------------------------------------------------------")
